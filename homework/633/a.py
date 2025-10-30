52e88be60fe78d98c796d8e258e69c7b84120215
AIlhomov <Ilhomovvahid@gmail.com>
2025-10-30 23:32:48 +0200

    Z-Algorithm

--- homework/633/a.py
+++ homework/633/a.py
@@ -1,1 +1,28 @@
+s = input()
 
+def solve(s):
+    l = r = 0
+    n = len(s)
+    z = [0] * n
+    z[0] = n #only fix
+
+    for i in range(1, n):
+        if i > r:
+            l = r = i
+            while r < n and s[r] == s[r-l]:
+                r += 1
+            z[i] = r - l
+            r -= 1
+        else:
+            rel_ind = i-l
+            if z[rel_ind] + i <= r:
+                z[i] = z[rel_ind]
+            else:
+                l = i
+                while r < n and s[r] == s[r-l]:
+                    r += 1
+                z[i] = r - l
+                r -= 1
+    return z
+
+print(*solve(s))
